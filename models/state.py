4f96b9f7e5c53ae816db74d000a312d6a0181f89
Antoniomary <amanso.totti@yahoo.com>
2024-03-23 10:13:02 -0700

    state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'states'
+
+        name = Column(
+                String(128),
+                nullable=False,
+            )
+
+        cities = relationship("City", cascade="all, delete", backref="states")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """returns the list of City instances with equal state_id"""
+            city = []
+            for each in storage.all(City).values():
+                if each["state_id"] == self.id:
+                    city.append(each)
+            return city
