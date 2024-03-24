49ccb0af96bf065d6399cf2532094ecb49a05b47
Antoniomary <amanso.totti@yahoo.com>
2024-03-23 22:52:50 -0700

    review.py

--- models/review.py
+++ models/review.py
@@ -5,7 +5,7 @@ from os import getenv
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     if getenv("HBNB_TYPE_STORAGE") == 'db':
         __tablename__ = "reviews"
