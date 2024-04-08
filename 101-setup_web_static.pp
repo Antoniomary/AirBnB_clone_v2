29a8332ebed9881f22abe6cafb3ed8e5298b068d
Antoniomary <amanso.totti@yahoo.com>
2024-04-07 17:04:11 -0700

    101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,51 +1,46 @@
 # sets up a web server for the deployment of web_static.
 
-package { 'update':
-  ensure   => latest,
-  provider => apt-get,
+$line = '\ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}'
+
+exec { 'update':
+  command  => 'sudo apt-get update',
+  path     => ['/usr/bin', '/usr/sbin'],
+  provider => shell,
 }
 
 package { 'nginx':
   ensure   => present,
-  provider => apt-get,
-  require  => Package['update'],
+  require  => Exec['update'],
 }
 
-file { '/data/web_static/shared':
-  ensure  => directory,
-  recurse => true,
+exec { '/usr/bin/mkdir -p /data/web_static/shared':
+  provider => shell,
 }
 
-file { '/data/web_static/releases/test':
-  ensure  => directory,
-  recurse => true,
+exec { '/usr/bin/mkdir -p /data/web_static/releases/test':
+  provider => shell,
 }
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => file,
-  content => '<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>',
+  content => "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>",
   owner   => 'root',
   group   => 'root',
   mode    => '0644',
-  require => File['/data/web_static/releases/test'],
 }
 
-exec { 'create_link':
-  command  => 'ln -sf /data/web_static/releases/test /data/web_static/current',
+exec { 'ln -sf /data/web_static/releases/test /data/web_static/current':
+  path     => ['/usr/bin', '/usr/sbin'],
   provider => shell,
-  require  => File['/data/web_static/releases/test'],
 }
 
-exec { 'change_owner':
-  command  => 'sudo chown -Rh ubuntu:ubuntu /data',
+exec { 'sudo chown -Rh ubuntu:ubuntu /data':
+  path     => ['/usr/bin', '/usr/sbin'],
   provider => shell,
-  require  => File['/data'],
 }
 
 exec { 'add_config':
-  command  => 'sed -i ' +
-  '"/server_name _;/ a \ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}"' +
-  ' /etc/nginx/sites-available/default',
+  command  => "sed -i '/server_name _;/ a ${line}' /etc/nginx/sites-available/default",
   provider => shell,
   require  => Package['nginx'],
 }
