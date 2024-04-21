c951e50c2150fd7f4a841bd0a75df9aaa7c424ee
Antoniomary <amanso.totti@yahoo.com>
2024-04-21 00:48:46 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,36 +23,37 @@ def do_deploy(archive_path):
     dir = file[:-4]
 
     # uncompressing the archive to a new directory on webserver
-    r = run('mkdir -p /data/web_static/releases/{}'.format(dir))
+    r = run('sudo mkdir -p /data/web_static/releases/{}'.format(dir))
     if r.failed:
         return False
-    r = run(f"tar -xzf /tmp/{file} -C /data/web_static/releases/{dir}")
+    r = run(f"sudo tar -xzf /tmp/{file} -C /data/web_static/releases/{dir}")
     if r.failed:
         return False
 
     # delete archive from the web server
-    r = run('rm /tmp/{}'.format(file))
+    r = run('sudo rm /tmp/{}'.format(file))
     if r.failed:
         return False
 
     # move files
-    r = run(f'mv /data/web_static/releases/{dir}/web_static/* \
-            /data/web_static/releases/{dir}')
+    r = run(f'sudo mv /data/web_static/releases/{dir}/web_static/* \
+/data/web_static/releases/{dir}')
     if r.failed:
         return False
 
     # delete folder from which files were moved
-    r = run(f'rm -rf /data/web_static/releases/{dir}/web_static')
+    r = run(f'sudo rm -rf /data/web_static/releases/{dir}/web_static')
     if r.failed:
         return False
 
     # delete old symlink
-    r = run('rm -rf /data/web_static/current')
+    r = run('sudo rm -rf /data/web_static/current')
     if r.failed:
         return False
 
     # creates a new symbolic link
-    r = run(f"ln -s /data/web_static/releases/{dir} /data/web_static/current")
+    r = run(f"sudo ln -s /data/web_static/releases/{dir} \
+/data/web_static/current")
     if r.failed:
         return False
 
