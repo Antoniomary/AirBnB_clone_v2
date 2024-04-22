7486f3f561059611887a05a6b91f520957a3b019
Antoniomary <amanso.totti@yahoo.com>
2024-04-21 17:53:33 -0700

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """displays Hello HBNB! as default index page"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """displays HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_c_text():
+    """displays C followed by the value of the text"""
+    formatted_text = text.replace('_', ' ')
+    return 'C ' + formatted_text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
