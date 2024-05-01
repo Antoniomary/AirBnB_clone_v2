6e887e89bbf658a2c48e5f5c74b8101132f8f63b
Antoniomary <amanso.totti@yahoo.com>
2024-04-30 17:22:04 -0700

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -33,17 +33,25 @@ class BaseModel:
             )
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+        """Instantiates a new model"""
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == 'updated_at':
+                    kwargs[k] = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+                elif k == 'created_at':
+                    kwargs[k] = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+                elif k == '__class__':
+                    del kwargs[k]
+            if not kwargs.get('created_at') and not kwargs.get('updated_at'):
+                kwargs['created_at'] = datetime.now()
+                kwargs['updated_at'] = datetime.now()
+            if not kwargs.get('id'):
+                kwargs['id'] = str(uuid.uuid4())
             self.__dict__.update(kwargs)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
