3a40f1167644bc7c02cbb31353402a00b40df15f
Antoniomary <amanso.totti@yahoo.com>
2024-05-04 12:52:48 -0700

    db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -35,12 +35,6 @@ class DBStorage:
 
     def all(self, cls=None):
         """query database session"""
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
         result = {}
         classes = {
             "User": User,
