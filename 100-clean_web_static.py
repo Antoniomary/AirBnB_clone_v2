6d056ef93732076a9f31a704728cafdf0e993d4e
Antoniomary <amanso.totti@yahoo.com>
2024-05-16 21:07:08 -0700

    new 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 """a fabric script that deletes out-of-date archives"""
 from fabric.api import env, local, run
+from sys import exit
 
 
-env.hosts = ['52.91.', '35.153.83.163']
+env.hosts = ['100.25.155.10', '54.90.5.78']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_clean(number=0):
     """deletes all unnecessary archives"""
+    try:
+    	number = int(number)
+    except ValueError:
+        print("Error: Invalid number")
+        exit(1)
+	
     if number < 0:
         return
 
@@ -19,4 +26,4 @@ def do_clean(number=0):
         number += 1
 
     local('ls -1t versions/ | tail +{} | xargs rm'.format(number))
-    run('ls -1t /data/web_static/releases | tail +{} | xargs rm'.format(number))
+    # run('ls -1t /data/web_static/releases | tail +{} | xargs rm'.format(number))
