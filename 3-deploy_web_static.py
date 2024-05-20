bc7005c894d953cc75fd7664bf4f10bc7e8fddf7
Antoniomary <amanso.totti@yahoo.com>
2024-05-19 21:21:39 -0700

    removed sudo also

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -38,23 +38,23 @@ def do_deploy(archive_path):
 
         # uncompressing the archive to a new directory on webserver
         new_dir = '/data/web_static/releases/{}'.format(dr)
-        run('sudo mkdir -p {}/'.format(new_dir))
-        run('sudo tar -xzf /tmp/{} -C {}/'.format(fil, new_dir))
+        run('mkdir -p {}/'.format(new_dir))
+        run('tar -xzf /tmp/{} -C {}/'.format(fil, new_dir))
 
         # delete archive from the web server
-        run('sudo rm /tmp/{}'.format(fil))
+        run('rm /tmp/{}'.format(fil))
 
         # move files
-        run('sudo mv {}/web_static/* {}/'.format(new_dir, new_dir))
+        run('mv {}/web_static/* {}/'.format(new_dir, new_dir))
 
         # delete folder from which files were moved
-        run('sudo rm -rf {}/web_static'.format(new_dir))
+        run('rm -rf {}/web_static'.format(new_dir))
 
         # delete old symlink
-        run('sudo rm -rf /data/web_static/current')
+        run('rm -rf /data/web_static/current')
 
         # creates a new symbolic link
-        run('sudo ln -s {} /data/web_static/current'.format(new_dir))
+        run('ln -s {} /data/web_static/current'.format(new_dir))
     except BaseException:
         return False
 
